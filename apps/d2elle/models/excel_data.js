aeaa86bc1ccb4ddc3599185036769ea89c1a067f
Darnell Brawner <darnell.brawner@me.com>
2011-03-18 18:34:58 -0400

    added first model file

---
+++ apps/d2elle/models/excel_data.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   D2elle.ExcelData
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals D2elle */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+D2elle.ExcelData = SC.Record.extend(
+/** @scope D2elle.ExcelData.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/d2elle/tests/models/excel_data.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   D2elle.ExcelData Unit Test
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals D2elle module test ok equals same stop start */
+
+module("D2elle.ExcelData");
+
+// TODO: Replace with real unit test for ExcelData
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
